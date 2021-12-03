2bb740b0999c3b095b2debbf38ef96436d9c9aee
User <User@192.168.81.61>
2021-12-03 11:18:05 +0530

    done by set2

---
+++ Set2JavaTestGit/Gson/UserSimple.java
@@ -0,0 +1,12 @@
+package Gson;
+
+public class UserSimple {
+
+	private String name;
+	private String email;
+	private int age;
+	System.out.println("Set2 changes");
+	public UserSimple(String name , String email, int age  ) {
+		
+	}
+}
